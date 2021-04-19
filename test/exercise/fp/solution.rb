6aef187aa5e50a67c70a4b542267ca70e6465eea
krivtsov <krivtsov.su@gmail.com>
2021-04-20 00:57:49 +0400

    rfix logic

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,16 +5,21 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(array)
-        total = array.each_with_object({ sum_rating_kinopoisk: 0, films_count: 0 }) do |film, memo|
-          count_country = film['country'] ? film['country'].split(',').length : 0
-          rating_kinopoisk = film['rating_kinopoisk'].to_f
-          if count_country > 1 && rating_kinopoisk.positive?
-            memo[:sum_rating_kinopoisk] += rating_kinopoisk
-            memo[:films_count] += 1
-          end
+        filtered_films =
+          array
+          .select { |film| film['rating_kinopoisk'].to_f.positive? }
+          .select { |film| film['country'] }
+          .select { |film| film['country'].split(',').length > 1 }
+
+        films_count = filtered_films.length
+
+        total_rating = filtered_films.reduce(0) do |memo, film|
+          memo += film['rating_kinopoisk'].to_f
           memo
         end
-        total[:sum_rating_kinopoisk] / total[:films_count]
+
+        avg_rating = total_rating / films_count
+        avg_rating
       end
 
       def chars_count(films, threshold)
